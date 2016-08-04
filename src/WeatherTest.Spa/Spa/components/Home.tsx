c1a1f4e3341585744ada9add152e6c957c4887f8
Mike Jones <mike.jones@abellearning.com>
2016-08-04 12:21:39 +0100

    Add new route to show weather information

--- src/WeatherTest.Spa/Spa/components/Home.tsx
+++ src/WeatherTest.Spa/Spa/components/Home.tsx
@@ -1,12 +1,31 @@
-﻿// A '.tsx' file enables JSX support in the TypeScript compiler, 
-// for more information see the following page on the TypeScript wiki:
-// https://github.com/Microsoft/TypeScript/wiki/JSX
-import * as React from 'react';
+﻿import * as React from 'react';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+import { provide } from 'redux-typed';
+import { Link } from 'react-router';
+import { ApplicationState }  from '../store';
+import * as LocationStore from '../store/Location';
+import * as CounterStore from '../store/Counter';
 
-export default class Home extends React.Component<any, void> {
+
+class Home extends React.Component<LocationProps, void> {
+
+
     public render() {
         return <div>
-            <h1>Hello, world!2</h1>
+            <h1>Weather Test</h1>
+            <h2>Enter Location: </h2>
+            <input onChange={ (event) => this.props.changeLocation(event.target.value) }  />
+            <button onClick={ () => this.props.showData() }>Go!</button>
         </div>;
     }
 }
+
+const provider = provide(
+    (state: ApplicationState) => state.location, // Select which part of global state maps to this component
+    LocationStore.actionCreators                 // Select which action creators should be exposed to this component
+);
+
+type LocationProps = typeof provider.allProps;
+export default provider.connect(Home);
+
--- src/WeatherTest.Spa/Spa/routes.tsx
+++ src/WeatherTest.Spa/Spa/routes.tsx
@@ -2,7 +2,11 @@
 import { Router, Route, HistoryBase } from 'react-router';
 import { Layout } from './components/Layout';
 import Home from './components/Home';
+import Weather from './components/Weather';
 
 export default <Route component={ Layout }>
     <Route path='/' components={{ body: Home }} />
+    <Route path='/weather' components={{ body: Weather }} >
+        <Route path='(:location)' /> { }
+    </Route>
 </Route>;
